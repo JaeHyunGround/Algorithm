52710759f44b6ef8aa38bee6fa3b5fbc2a9d0361
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-03-18 19:50:17 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9416 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+// bfs를 사용하면 될듯
+
+const N = +input.shift();
+const M = +input.shift();
+
+const graph = [...Array(N + 1)].map(() => []);
+const edges = input.map((line) => line.split(" ").map(Number));
+edges.forEach(([from, to]) => {
+  graph[from].push(to);
+  graph[to].push(from);
+});
+
+const dfs = (startNode) => {
+  const queue = [startNode];
+  const visitedNodes = [];
+  while (queue.length) {
+    const node = queue.pop();
+    if (!visitedNodes.includes(node)) {
+      visitedNodes.push(node);
+      queue.push(...graph[node]);
+    }
+  }
+
+  return visitedNodes;
+};
+
+console.log(dfs(1).length - 1);
