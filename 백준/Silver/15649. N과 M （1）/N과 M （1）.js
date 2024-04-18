45519d70dadbb827af78890ac22ef95a2722658b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-04-18 14:23:17 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 17976 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -7,11 +7,11 @@ const M = +input.shift();
 
 const visited = new Array(N + 1);
 let output = [];
-let result = "";
+let answer = "";
 
 function dfs(cnt) {
   if (cnt === M) {
-    result += `${output.join(" ")}\n`;
+    answer += `${output.join(" ")}\n`;
     return;
   }
 
@@ -26,4 +26,4 @@ function dfs(cnt) {
 }
 
 dfs(0);
-console.log(result.trim());
+console.log(answer.trim());
