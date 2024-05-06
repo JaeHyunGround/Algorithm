8aef3f2708757557710d73d105906273c8534130
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-06 21:49:44 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,24 +1,9 @@
 function solution(nums) {
     let answer = 0;
-    let N = nums.length;
+    let halfN = nums.length / 2
+    let newNums = [...new Set(nums)]
     
-    let myMap = new Map();
-    nums.forEach((phonekemon) => {
-        if (!myMap.get(phonekemon)) {
-            myMap.set(phonekemon, 1)
-        } else {
-            myMap.set(phonekemon, myMap.get(phonekemon) + 1)
-        }
-    })
+    answer = newNums.length >= halfN ? halfN : newNums.length
     
-    let newNums = [...myMap.keys()]
-    
-    if (newNums.length >= N/2) {
-        answer = N/2
-    } else {
-        answer = newNums.length
-    }
-    
-    
-    return answer;
+    return answer
 }
