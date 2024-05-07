dd553dfd32f3a7a35e309abe8477314725619e2b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-07 20:04:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 30.63 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,8 @@
 function solution(participant, completion) {
     let myMap = new Map();
     participant.forEach((people) => {
-        if (!myMap.get(people)) {
-            myMap.set(people, 1)
-        } else {
-            myMap.set(people, myMap.get(people) + 1)
-        }
+        if (!myMap.get(people)) myMap.set(people, 1) 
+        else myMap.set(people, myMap.get(people) + 1)
     })
     completion.forEach((people) => {
         if (myMap.get(people)) myMap.set(people, myMap.get(people) - 1)
