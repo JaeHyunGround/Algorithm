5cdc310bc0fd93d27c085d960d66cc734b70b7b5
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-07 15:11:06 +0900

    [Silver III] Title: 거북이, Time: 940 ms, Memory: 21244 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.js
+++ 백준/Silver/8911. 거북이/거북이.js
@@ -10,37 +10,49 @@ while (input.length > 0) {
   let map = [];
   let x = 0;
   let y = 0;
+  let maxX = 0;
+  let maxY = 0;
+  let minX = 0;
+  let minY = 0;
 
   cases.forEach((d) => {
     switch (d) {
       case "F":
         if (currentDirection === "N") {
           x++;
-          map.push([x, y]);
+          if (maxX < x) maxX = x;
+          if (minX > x) minX = x;
         } else if (currentDirection === "S") {
           x--;
-          map.push([x, y]);
+          if (maxX < x) maxX = x;
+          if (minX > x) minX = x;
         } else if (currentDirection === "E") {
           y++;
-          map.push([x, y]);
+          if (maxY < y) maxY = y;
+          if (minY > y) minY = y;
         } else if (currentDirection === "W") {
           y--;
-          map.push([x, y]);
+          if (maxY < y) maxY = y;
+          if (minY > y) minY = y;
         }
         break;
       case "B":
         if (currentDirection === "N") {
           x--;
-          map.push([x, y]);
+          if (maxX < x) maxX = x;
+          if (minX > x) minX = x;
         } else if (currentDirection === "S") {
           x++;
-          map.push([x, y]);
+          if (maxX < x) maxX = x;
+          if (minX > x) minX = x;
         } else if (currentDirection === "E") {
           y--;
-          map.push([x, y]);
+          if (maxY < y) maxY = y;
+          if (minY > y) minY = y;
         } else if (currentDirection === "W") {
           y++;
-          map.push([x, y]);
+          if (maxY < y) maxY = y;
+          if (minY > y) minY = y;
         }
         break;
       case "L":
@@ -66,18 +78,6 @@ while (input.length > 0) {
         }
         break;
     }
-  });
-
-  let maxX = 0;
-  let maxY = 0;
-  let minX = 0;
-  let minY = 0;
-
-  map.forEach(([x, y]) => {
-    if (maxX < x) maxX = x;
-    if (maxY < y) maxY = y;
-    if (minX > x) minX = x;
-    if (minY > y) minY = y;
   });
 
   console.log((maxX - minX) * (maxY - minY));
