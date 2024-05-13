caa99bb2b29ed3cb072fe715fce8b761eadd848e
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-13 15:33:38 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -3,16 +3,23 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
 
 const testCase = input[0];
+const answer = [];
 
-for (let i = 1; i <= testCase; i++) {
-  const dp = new Array(input[i] + 1).fill(0);
+function solution(num) {
+  const dp = new Array(num + 1).fill(0);
   dp[1] = 1;
   dp[2] = 2;
   dp[3] = 4;
 
-  for (let j = 4; j <= input[i]; j++) {
-    dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+  for (let i = 4; i <= num; i++) {
+    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
   }
 
-  console.log(dp[input[i]]);
+  return dp[num];
 }
+
+for (let i = 1; i <= testCase; i++) {
+  answer.push(solution(input[i]));
+}
+
+console.log(answer.join("\n"));
