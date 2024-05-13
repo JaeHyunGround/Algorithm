04e812f2019c933175a5e2193607a795cf3a1801
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-13 15:12:00 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 17456 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -3,19 +3,15 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = Number(fs.readFileSync(filePath).toString().trim());
 
 const dp = new Array(input + 1).fill(0);
-dp[1] = 0;
 
 for (let i = 2; i <= input; i++) {
   dp[i] = dp[i - 1] + 1;
 
   if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
   }
   if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
-  }
-  if (i % 3 === 0 && i % 2 === 0) {
-    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1, dp[i / 2] + 1);
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
   }
 }
 
