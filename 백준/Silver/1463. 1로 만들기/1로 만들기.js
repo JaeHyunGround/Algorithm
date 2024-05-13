024a7bce0c942ec70a0437467c7e95f9a76733b6
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-13 15:09:03 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,26 +1,22 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number)[0];
+let input = Number(fs.readFileSync(filePath).toString().trim());
 
-const num = Number(input);
+const dp = new Array(input + 1).fill(0);
+dp[1] = 0;
 
-const dp = new Array(num + 1).fill(0);
-
-for (let i = 2; i <= num; i++) {
+for (let i = 2; i <= input; i++) {
   dp[i] = dp[i - 1] + 1;
 
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-  }
-
   if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
+  }
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
+  }
+  if (i % 3 === 0 && i % 2 === 0) {
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1, dp[i / 2] + 1);
   }
 }
 
-console.log(dp[num]);
+console.log(dp[input]);
