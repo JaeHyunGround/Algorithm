32ed736219dfb12f577116298ef68558041db079
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-24 23:01:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 588 ms, Memory: 90744 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -2,51 +2,55 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let [N, M] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
 
-class Queue {
-  constructor() {
-    this.queue = [];
-    this.front = 0;
-    this.rear = 0;
+function solution(start, target, count) {
+  class Queue {
+    constructor() {
+      this.queue = [];
+      this.front = 0;
+      this.rear = 0;
+    }
+
+    enqueue(value) {
+      this.queue[this.rear++] = value;
+    }
+
+    dequeue() {
+      const value = this.queue[this.front];
+      delete this.queue[this.front];
+      this.front++;
+      return value;
+    }
+
+    size() {
+      return this.rear - this.front;
+    }
   }
 
-  enqueue(value) {
-    this.queue[this.rear++] = value;
-  }
+  function bfs(start, target, count) {
+    const queue = new Queue();
+    const visited = new Array(100001).fill(false);
+    queue.enqueue([start, count]);
 
-  dequeue() {
-    const value = this.queue[this.front];
-    delete this.queue[this.front];
-    this.front++;
-    return value;
-  }
+    while (queue.size()) {
+      let [currentNumber, currentCount] = queue.dequeue();
 
-  size() {
-    return this.rear - this.front;
-  }
-}
-
-function bfs(start, count, target) {
-  const queue = new Queue();
-  const visited = new Array(100001).fill(false);
-  queue.enqueue([start, count]);
-
-  while (queue.size()) {
-    let [currentNumber, currentCount] = queue.dequeue();
-
-    if (!visited[currentNumber]) {
-      visited[currentNumber] = true;
-      if (currentNumber === target) {
-        return currentCount;
-      } else {
-        if (currentNumber + 1 <= 100000)
-          queue.enqueue([currentNumber + 1, currentCount + 1]);
-        if (currentNumber - 1 <= 100000)
-          queue.enqueue([currentNumber - 1, currentCount + 1]);
-        if (currentNumber * 2 <= 100000)
-          queue.enqueue([currentNumber * 2, currentCount + 1]);
+      if (!visited[currentNumber]) {
+        visited[currentNumber] = true;
+        if (currentNumber === target) {
+          return currentCount;
+        } else {
+          if (currentNumber + 1 <= 100000)
+            queue.enqueue([currentNumber + 1, currentCount + 1]);
+          if (currentNumber - 1 <= 100000)
+            queue.enqueue([currentNumber - 1, currentCount + 1]);
+          if (currentNumber * 2 <= 100000)
+            queue.enqueue([currentNumber * 2, currentCount + 1]);
+        }
       }
     }
   }
+
+  return bfs(start, target, count);
 }
 
-console.log(bfs(N, 0, M));
+console.log(solution(N, M, 0));
