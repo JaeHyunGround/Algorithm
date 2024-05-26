baabf11d23e7ca8e4ee3814ca9973dafa6b8e2e5
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-05-27 02:23:37 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.js
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.js
@@ -2,12 +2,18 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = Number(fs.readFileSync(filePath).toString().trim());
 
-const dp = [];
-dp[0] = 1;
-dp[1] = 1;
+let limit = 1000000007;
 
-for (let i = 2; i <= input; i++) {
-  dp[i] = (dp[i - 2] + dp[i - 1] + 1) % 1000000007;
+function solution(num) {
+  const dp = [];
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= num; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1] + 1) % limit;
+  }
+
+  return dp[num];
 }
 
-console.log(dp[input]);
+console.log(solution(input));
