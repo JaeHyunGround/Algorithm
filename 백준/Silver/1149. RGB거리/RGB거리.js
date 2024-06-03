a434ba62ba2a484c7fe7777e25bc92fac8cd693b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-06-03 16:15:31 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.js
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -7,9 +7,9 @@ const cost = input.map((line) => line.split(" ").map(Number));
 const dp = cost;
 
 for (let i = 1; i < N; i++) {
-  dp[i][0] = cost[i][0] + Math.min(cost[i - 1][1], cost[i - 1][2]);
-  dp[i][1] = cost[i][1] + Math.min(cost[i - 1][0], cost[i - 1][2]);
-  dp[i][2] = cost[i][2] + Math.min(cost[i - 1][0], cost[i - 1][1]);
+  dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+  dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+  dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
 }
 
 console.log(Math.min(...dp[N - 1]));
