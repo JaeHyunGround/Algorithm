06d786b072aea0b82e2f978b0326224599de7d17
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-06-10 13:25:07 +0900

    [Silver I] Title: 회의실 배정, Time: 416 ms, Memory: 45940 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -7,20 +7,24 @@ let [N, ...input] = fs
   .split("\n")
   .map((line) => line.split(" ").map(Number));
 
-const sortArr = input.sort((a, b) => {
-  if (a[1] === b[1]) return a[0] - b[0];
-  else return a[1] - b[1];
-});
+const solution = (timeTable) => {
+  const sortTable = timeTable.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
 
-let count = 0;
-let answer = 0;
-sortArr.forEach((line) => {
-  const [start, finish] = line;
+  let count = 0; // 끝나는 시간을 저장
+  let answer = 0;
+  sortTable.forEach((line) => {
+    const [start, finish] = line;
 
-  if (start >= count) {
-    answer += 1;
-    count = finish;
-  }
-});
+    if (start >= count) {
+      answer += 1;
+      count = finish;
+    }
+  });
 
-console.log(answer);
+  return answer;
+};
+
+console.log(solution(input));
