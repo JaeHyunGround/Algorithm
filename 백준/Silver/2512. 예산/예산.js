03365b7e141c6e8de9c0c35617e93e8c06d5739e
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-06-13 14:26:13 +0900

    [Silver II] Title: 예산, Time: 192 ms, Memory: 11780 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input.shift();
+const M = +input.pop();
+const costArr = input[0].split(" ").map(Number);
+
+function binarySearch(input, N, M) {
+  input.sort((a, b) => a - b);
+  let left = 1;
+  let right = input[N - 1];
+  let mid = Math.floor((right + left) / 2);
+  let newCostArr = [];
+  let totalCost = 0;
+
+  while (left <= right) {
+    newCostArr = costArr.map((num) => (num >= mid ? mid : num));
+    totalCost = newCostArr.reduce((acc, cur) => acc + cur, 0);
+
+    if (totalCost <= M) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+    mid = Math.floor((right + left) / 2);
+  }
+
+  return Math.max(right);
+}
+
+console.log(binarySearch(costArr, N, M));
