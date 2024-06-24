0976d542b9e667ed548ac595f34b3372c279b0d7
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-06-24 16:53:33 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,22 @@
+function solution(want, number, discount) {
+    let answer = 0;
+    
+    for (let j = 0; j <= discount.length - 10; j++) {
+        let compaerNumber = [...number]
+        let count = 0;
+        for (let k = j; k < j + 10; k++) {
+            if (want.indexOf(discount[k]) !== -1 && compaerNumber[want.indexOf(discount[k])] !== 0) {
+                compaerNumber[want.indexOf(discount[k])] = compaerNumber[want.indexOf(discount[k])] - 1
+                count += 1
+            } else {
+                count = 0;
+            }
+        }
+        if (count >= 10) answer += 1
+    }
+    
+    
+    
+    
+    return answer;
+}
