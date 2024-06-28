0848d546e81cda12eb86c911865712cbbbb6d878
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-06-28 17:34:29 +0900

    [Silver III] Title: N과 M (1), Time: 196 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,29 +1,34 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split(" ");
+let [N, M] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
 
-const N = +input.shift();
-const M = +input.shift();
+function solution(N, M) {
+  const visited = new Array(N + 1); // 방문을 확인하기 위한 배열
+  const output = []; // 하나하나 돌면서 경우를 확인 할 빈 배열
+  let answer = ""; // 최종 정답
 
-const visited = new Array(N + 1);
-let output = [];
-let answer = "";
+  function dfs(depth) {
+    // 가지치기 조건
+    // output의 길이 = depth가 조건 M을 만족한다면 최종 정답에 추가
+    if (depth === M) {
+      answer += `${output.join(" ")}\n`;
+      return;
+    }
 
-function dfs(cnt) {
-  if (cnt === M) {
-    answer += `${output.join(" ")}\n`;
-    return;
+    // for문을 돌면서 재귀로 경우를 하나하나 파악
+    for (let i = 1; i < N + 1; i++) {
+      if (visited[i] === true) continue;
+      visited[i] = true;
+      output.push(i);
+      dfs(depth + 1);
+      output.pop();
+      visited[i] = false;
+    }
   }
 
-  for (let i = 1; i <= N; i++) {
-    if (visited[i] === true) continue;
-    visited[i] = true;
-    output.push(i);
-    dfs(cnt + 1);
-    output.pop();
-    visited[i] = false;
-  }
+  dfs(0);
+  console.log(answer.trim());
 }
 
-dfs(0);
-console.log(answer.trim());
+solution(N, M);
+// 백트래킹은 조합이나 수열을 구할 때 쓸 수 있다.
