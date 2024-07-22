01557cc345c87777806e91d64409d822e68eaf2b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-07-22 19:26:57 +0900

    [Gold IV] Title: 공유기 설치, Time: 324 ms, Memory: 45156 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.js
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -5,24 +5,31 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [N, C] = input.shift().split(" ").map(Number);
 const house = input.map(Number).sort((a, b) => a - b);
 
-// 이진 탐색(매개변수 탐색)
-let start = 1; // 나올 수 있는 최소값
-let end = house[house.length - 1]; // 나올 수 있는 최댓값
-let mid = Math.floor((start + end) / 2); // 가장 인접한 두 공유기 사이의 최대 거리
+const binarySearch = (arr) => {
+  let start = 1;
+  let end = arr[arr.length - 1];
+  let mid = Math.floor((start + end) / 2);
 
-while (start <= end) {
-  let count = 1;
-  let prev = house[0];
-  for (const cur of house) {
-    if (cur - prev < mid) continue;
-    prev = cur;
-    count += 1;
+  while (start <= end) {
+    let count = 1;
+    let num = arr[0];
+
+    for (const cur of arr) {
+      if (cur - num < mid) continue;
+      num = cur;
+      count += 1;
+    }
+
+    if (count < C) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+
+    mid = Math.floor((start + end) / 2);
   }
 
-  if (count < C) end = mid - 1;
-  else start = mid + 1;
+  return end;
+};
 
-  mid = Math.floor((start + end) / 2);
-}
-
-console.log(end);
+console.log(binarySearch(house));
