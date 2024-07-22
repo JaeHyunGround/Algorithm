43f6adbdcca47e058ce15c5c0354944e54b60bdd
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-07-22 19:46:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 320 ms, Memory: 44916 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.js
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -9,6 +9,7 @@ const binarySearch = (arr) => {
   let start = 1;
   let end = arr[arr.length - 1];
   let mid = Math.floor((start + end) / 2);
+  let answer = end;
 
   while (start <= end) {
     let count = 1;
@@ -23,13 +24,14 @@ const binarySearch = (arr) => {
     if (count < C) {
       end = mid - 1;
     } else {
+      answer = mid; // 최댓값을 구하는 문제이니깐 mid가 커져갈 때 answer을 업데이트
       start = mid + 1;
     }
 
     mid = Math.floor((start + end) / 2);
   }
 
-  return end;
+  return answer;
 };
 
 console.log(binarySearch(house));
