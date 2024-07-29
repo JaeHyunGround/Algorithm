c8842359732662fa8b46fe674483f02c4d95d6f7
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-07-29 17:06:59 +0900

    [level 2] Title: H-Index, Time: 4.01 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -2,8 +2,10 @@ function solution(citations) {
     let answer = 0;
     citations.sort((a, b) => b - a)
     
+    console.log(citations)
+    
     for (let i = 0; i < citations.length; i++) {
-        if (citations[i] > i) answer += 1
+        if (citations[i] >= i + 1) answer += 1
         else break;
     }
     
