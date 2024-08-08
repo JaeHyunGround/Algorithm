a11a6d2a00018f45f5d47bb9111b0fe4862931a9
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-08-08 23:26:28 +0900

    [level 1] Title: 모의고사, Time: 3.39 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,19 +1,26 @@
 function solution(answers) {
-    let answer = [];
+    const answer = [];
+    const result = [];
     
-    const info = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    const answerCounter = Array.from({length: 3}, () => 0)
-    
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < answers.length; j++) {
-            if (answers[j] === info[i][j % info[i].length]) answerCounter[i] += 1
+    const arr = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    arr.forEach((line) => {
+        let count = 0
+        let idx = 0;
+        for (let i = 0; i < answers.length; i++) {
+            if (idx >= line.length) idx = 0;
+            
+            if (answers[i] === line[idx]) {
+                count += 1;
+            }
+            idx++;
         }
-    }
-    
-    const max = Math.max(...answerCounter)
-    answerCounter.forEach((num, index) => {
-        if (num === max) answer.push(index + 1)
+        result.push(count)
     })
+    
+    let max = Math.max(...result)
+    for (let i = 0; i < 3; i++) {
+        if (max === result[i]) answer.push(i + 1)
+    }
     
     return answer;
 }
