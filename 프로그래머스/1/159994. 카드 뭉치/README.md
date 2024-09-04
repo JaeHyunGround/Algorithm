90b780d4be36d0e6c9d4dc120cc683a184f31c58
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-09-04 16:53:17 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.22 ms
+메모리: 33.5 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 23:57:58
+2024년 09월 04일 16:53:14
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,21 +1,16 @@
 function solution(cards1, cards2, goal) {
     let answer = "Yes";
     
-    for (let i = 0; i < goal.length; i++) {
-        if (cards1.indexOf(goal[i]) !== -1 && cards2.indexOf(goal[i]) !== -1) {
-            answer = "No"
-            break;
-        } else {
-            if (cards1.indexOf(goal[i]) !== 0 && cards2.indexOf(goal[i]) !== 0) {
-                answer = "No"
-                break;
+    for (const word of goal) {
+        if (cards1[0] === word || cards2[0] === word) {
+            if (cards1[0] === word) {
+                cards1.shift();
             } else {
-                if (cards1.indexOf(goal[i]) === 0) {
-                    cards1.splice(0, 1)
-                } else {
-                    cards2.splice(0, 1)
-                }
+                cards2.shift();
             }
+        } else {
+            answer = "No"
+            return answer
         }
     }
     
