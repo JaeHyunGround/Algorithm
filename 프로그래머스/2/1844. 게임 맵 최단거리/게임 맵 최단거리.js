bd3d7f09312e026e0496d1b90719a9f840cdcdd6
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-09-07 18:55:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 44.54 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -6,13 +6,13 @@ class Queue {
     }
     
     enqueue(value) {
-        this.queue[this.rear++] = value
+        this.queue[this.rear++] = value;
     }
     
     dequeue() {
-        const value = this.queue[this.front]
+        const value = this.queue[this.front];
         delete this.queue[this.front]
-        this.front += 1
+        this.front += 1 
         return value
     }
     
@@ -26,27 +26,36 @@ class Queue {
 }
 
 function solution(maps) {
-    const queue = new Queue()
-    const [n, m] = [maps.length, maps[0].length];
-    const [targetX, targetY] = [n - 1, m - 1]
-    const dx = [-1, 1, 0, 0]
-    const dy = [0, 0, -1, 1]
-    queue.enqueue([0, 0, 1])
-
-    while (queue.size() > 0) {
-        const [x, y, cost] = queue.dequeue();
-        if (x === targetX && y === targetY) return cost;
+    const c = maps.length;
+    const r = maps[0].length
+    
+    const dc = [0, 0, 1, -1]
+    const dr = [1, -1, 0, 0]
+    
+    const bfs = (start) => {
+        const queue = new Queue();
+        const visited = Array.from({length: c}, () => Array(r).fill([false, 0]))
+        queue.enqueue(start)
         
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i]
-            const ny = y + dy[i]
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (maps[nx][ny] === 1) {
-                queue.enqueue([nx, ny, cost + 1])
-                maps[nx][ny] = 0;
+        while (queue.size() > 0) {
+            const [curC, curR, cost] = queue.dequeue();
+            
+            if (!visited[curC][curR][0]) {
+                visited[curC][curR] = [true, cost]
+                
+                for (let i = 0; i < 4; i++) {
+                    const nextC = curC + dc[i]
+                    const nextR = curR + dr[i]
+                    
+                    if (nextC < 0 || nextR < 0 || nextC >= c || nextR >= r || maps[nextC][nextR] === 0) continue;
+                    
+                    queue.enqueue([nextC, nextR, cost + 1])
+                }
             }
         }
+        
+        return visited[c - 1][r - 1][0] ? visited[c - 1][r - 1][1] : -1
     }
     
-    return -1
+    return bfs([0, 0, 1])
 }
