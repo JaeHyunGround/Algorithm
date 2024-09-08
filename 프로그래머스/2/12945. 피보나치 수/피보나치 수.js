5794784c930ce411dd8d584a2b8593db4a98b9e8
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-09-08 13:31:27 +0900

    [level 2] Title: 피보나치 수, Time: 3.94 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    const dp = new Array(n + 1).fill(0)
+    const dp = Array(n).fill(0)
     dp[0] = 0
     dp[1] = 1
+    
     for (let i = 2; i <= n; i++) {
-        dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
     }
-    return dp[n];
+    return dp[n]
 }
