737224dc99bccce40d33cdc09ff3946792549b53
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-09-09 14:33:56 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -2,15 +2,14 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let [N, M] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
 
-function solution(N, M) {
-  const visited = new Array(N + 1);
+const solution = (N, M) => {
+  const visited = Array.from({ length: N + 1 }, () => false);
   const output = [];
   const answer = [];
 
   const dfs = (depth) => {
     if (depth === M) {
       answer.push(output.join(" "));
-      return;
     }
 
     for (let i = 1; i <= N; i++) {
@@ -25,6 +24,6 @@ function solution(N, M) {
 
   dfs(0);
   return answer.join("\n");
-}
+};
 
 console.log(solution(N, M));
