81ed8529cf0405b84ff2abb8f73c304806d69ee3
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-09-27 17:25:11 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,10 +1,8 @@
 function solution(strings, n) {
     const answer = strings.sort((a, b) => {
-        if (a[n] > b[n]) return 1;
-        else if (a[n] <b[n]) return -1;
+        if (a[n] === b[n]) return a > b ? 1 : -1
         else {
-            if (a > b) return 1;
-            else return -1
+            return a[n] > b[n] ? 1 : -1
         }
     })
     return answer;
