09e373ca9f4c5db9990f50630a1d2ee1eee274d7
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-10-04 22:08:29 +0900

    [level 2] Title: 타겟 넘버, Time: 14.76 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 16.54 ms
+메모리: 35.6 MB, 시간: 14.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 21:28:32
+2024년 10월 04일 22:08:27
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,25 @@
 function solution(numbers, target) {
-    let answer = 0;
-    let length = numbers.length
-    
-    function dfs(sum, idx) {
-        if (idx === length - 1) {
-            if (sum === target) {
-                answer += 1;
-            }
-            return;
-        }
-        
-        dfs(sum + numbers[idx + 1], idx + 1)
-        dfs(sum - numbers[idx + 1], idx + 1)
+  let answer = 0;
+
+  function dfs(index, sum) {
+    // 모든 숫자를 다 처리했을 때
+    if (index === numbers.length) {
+      // 타겟 넘버와 일치하면 경우의 수 증가
+      if (sum === target) {
+        answer++;
+      }
+      return;
     }
-    
-    dfs(numbers[0], 0)
-    dfs(-numbers[0], 0)
-    
-    return answer;
+
+    // 현재 index의 숫자를 더하는 경우
+    dfs(index + 1, sum + numbers[index]);
+
+    // 현재 index의 숫자를 빼는 경우
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  // 탐색 시작
+  dfs(0, 0);
+
+  return answer;
 }
