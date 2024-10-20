8a29c68360494747c7124a30837a524f1a072cfa
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-10-20 15:09:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 372 ms, Memory: 92412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -5,11 +5,15 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [N, K] = input.shift().split(" ").map(Number);
 const dp = Array.from({ length: N + 1 }, () => Array(K + 1).fill(0));
 
-for (let i = 1; i < N + 1; i++) {
+for (let i = 1; i <= N; i++) {
   const [W, V] = input[i - 1].split(" ").map(Number);
+
   for (let j = 1; j <= K; j++) {
-    if (j - W >= 0) dp[i][j] = Math.max(dp[i - 1][j - W] + V, dp[i - 1][j]);
-    else dp[i][j] = dp[i - 1][j];
+    if (j - W >= 0) {
+      dp[i][j] = Math.max(dp[i - 1][j - W] + V, dp[i - 1][j]);
+    } else {
+      dp[i][j] = dp[i - 1][j];
+    }
   }
 }
 
