013e85fcae8888e6b7e52a78444fda333531b80e
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2025-08-27 10:46:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.77 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,28 @@
-function solution(arr) {
-    const answer = [];
-
-    for (const num of arr) {
-        if (answer.length > 0 && num === answer[answer.length - 1]) {
-            continue;
-        } else {
-            answer.push(num)
-        }
+const solution = (arr) => {
+    const answer = [arr[0]];
+    let top = arr[0]
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (top === arr[i]) continue;
+        
+        answer.push(arr[i])
+        top = arr[i]
     }
     
-    return answer;
+    return answer
 }
+
+
+// function solution(arr) {
+//     const answer = [];
+
+//     for (const num of arr) {
+//         if (answer.length > 0 && num === answer[answer.length - 1]) {
+//             continue;
+//         } else {
+//             answer.push(num)
+//         }
+//     }
+    
+//     return answer;
+// }
