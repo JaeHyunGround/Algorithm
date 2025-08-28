7006ed01605667840ba8af44a1d979ac5e803150
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2025-08-28 18:42:47 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,17 @@
+const solution = (numbers) => {
+ return numbers.reduce((acc, crr) => acc - crr, 45)
+}
+
+// function solution(numbers) {
+//     let no_num = []; // 없는 숫자를 담는 배열
+    
+//     for (var i = 0; i <= 9; i++) {
+//         if(numbers.indexOf(i) == -1) {
+//             no_num.push(i);
+//         }
+//     }
+    
+//     let answer = no_num.reduce((a, b) => a + b);
+    
+//     return answer;
+// }
