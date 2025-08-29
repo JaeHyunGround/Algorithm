957d1a53b12e45292d6c8c7b7e682c2bf091335a
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2025-08-29 11:47:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,21 @@
+const solution = (phone_number) => {
+    const lastFourNumber = phone_number.slice(phone_number.length - 4)
+    const star = Array.from({length: phone_number.length - lastFourNumber.length}, () => '*').join("")
+    
+    
+    return star + lastFourNumber
+}
+
+// function solution(phone_number) {
+    
+//     let answer = [];
+    
+//     for(var i = 0; i < (phone_number.length - 4); i++) {
+//         answer.push("*");
+//     }
+//     for (var j = (phone_number.length - 4); j < phone_number.length; j++) {
+//         answer.push(phone_number[j]);
+//     }
+    
+//     return answer.join("");
+// }
