28066a0eca07b6448518f8c1c6ee7d8f0ce747d5
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2025-09-03 17:22:27 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,13 @@
-function solution(s) {
-    let answer = [];
-    let input = s.split(" ").map(Number)
+const solution = (s) => {
+    const numArray = s.split(" ").map(Number).sort((a, b) => a - b)
     
-    answer.push(input.reduce((a, b) => Math.min(a, b)), input.reduce((a, b) => Math.max(a, b))) 
-    
-    return answer.join(" ");
+    return `${numArray[0]} ${numArray[numArray.length - 1]}`
 }
+
+// function solution(s) {
+//     let answer = [];
+//     let input = s.split(" ").map(Number)
+//     answer.push(input.reduce((a, b) => Math.min(a, b)), input.reduce((a, b) => Math.max(a, b))) 
+    
+//     return answer.join(" ");
+// }
