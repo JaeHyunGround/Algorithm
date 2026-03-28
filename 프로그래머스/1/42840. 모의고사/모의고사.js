03fc62cb43b0ff3cb5ade965b48a5543488fb1c5
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-03-28 21:33:31 +0900

    [level 1] Title: 모의고사, Time: 28.74 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,22 @@
 function solution(answers) {
     const answer = [];
-    const result = [];
+    const result = [0, 0, 0];
     
     const arr = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    arr.forEach((line) => {
-        let count = 0
-        for (let i = 0; i < answers.length; i++) {          
-            if (answers[i] === line[i % line.length]) {
-                count += 1;
-            }
+    
+    arr.forEach((line, index) => {
+        let score = 0;
+        for (let i = 0; i < answers.length; i++) {
+            if (answers[i] === line[i % line.length]) result[index] += 1 
         }
-        result.push(count)
     })
     
     let max = Math.max(...result)
-    for (let i = 0; i < 3; i++) {
-        if (max === result[i]) answer.push(i + 1)
+    
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] === max) answer.push(i + 1)
     }
+    
     
     return answer;
 }
