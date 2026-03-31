e6efa6489519eed387c17b6f0f9defb2493041c6
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-03-31 10:42:36 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,4 @@
 function solution(n) {
-    const answer = String(n).split('').map(Number).reduce((acc, cur) => acc + cur)
+    const answer = n.toString().split('').map(Number).reduce((acc, cur) => acc + cur)
     return answer;
 }
