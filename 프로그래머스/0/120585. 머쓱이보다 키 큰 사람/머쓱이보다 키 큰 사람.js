ab1b87aa2d48a7e2068b7fda9d04883e5d3bda78
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-01 12:32:10 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,13 +1,4 @@
 function solution(array, height) {
-    let answer = array.length;
-    const sortArr = array.sort((a, b) => a - b)
-    
-    for (let i = 0; i < sortArr.length; i++) {
-        if (height < sortArr[i]) {
-            break;
-        }
-        answer -= 1
-    }
-    
-    return answer
+    let answer = array.filter(item => item > height)
+    return answer.length
 }
