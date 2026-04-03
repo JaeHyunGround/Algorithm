2f4328a90428d0a499df2d6a498e2c2179a051f0
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-03 18:29:25 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,6 +1,3 @@
 def solution(my_string, letter):
-    answer = ''
-    for string in my_string:
-        if string != letter:
-            answer += string
+    answer = my_string.replace(letter, '')
     return answer
