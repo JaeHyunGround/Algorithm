1a9722711fa0609f3f7aef18d1655f94710e3522
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-07 12:18:36 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,11 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+
+    for i in range(len(d)):
+        if budget - d[i] < 0:
+            break;
+        answer += 1
+        budget -= d[i]
+    
+    return answer
