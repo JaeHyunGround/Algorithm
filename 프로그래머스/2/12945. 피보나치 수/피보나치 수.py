478d1eea5a98bee7369544acbc96ae192bed4f06
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-07 11:19:54 +0900

    [level 2] Title: 피보나치 수, Time: 19.26 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -4,6 +4,6 @@ def solution(n):
     dp[2] = 1;
 
     for i in range(3, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
     
-    return dp[n] % 1234567
+    return dp[n]
