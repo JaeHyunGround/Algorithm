a7eff398b6adf852d479be51253c609def25a33c
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-08 16:26:09 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ''
+    mid = len(s) // 2
+    
+    if len(s) % 2 == 0:
+        answer = s[mid - 1] + s[mid]
+    else:
+        answer = s[mid]
+        
+    
+    return answer
+
+
+# 홀 짝 구분해서
