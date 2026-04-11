45d81992c803abbb2a17fb36a244c723aaa49600
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2026-04-11 20:39:52 +0900

    [Gold III] Title: 경사로, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,40 +1,37 @@
 N, L = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(N)]
 
-def check(line):
+def count(line):
     used = [False] * N
 
     for i in range(N - 1):
         diff = line[i + 1] - line[i]
 
-        if diff == 0:
-            continue
-        elif diff == -1:  # 낮아짐 → 오른쪽에 경사로
+        if diff == -1:
             for j in range(L):
                 if i + 1 + j >= N or line[i + 1 + j] != line[i + 1] or used[i + 1 + j]:
                     return False
                 used[i + 1 + j] = True
-        elif diff == 1:   # 높아짐 → 왼쪽에 경사로
+        elif diff == 1:
             for j in range(L):
                 if i - j < 0 or line[i - j] != line[i] or used[i - j]:
                     return False
                 used[i - j] = True
-        else:             # 차이 2 이상
+        elif diff == 0:
+            continue
+        else:
             return False
-
+    
     return True
 
-count = 0
-
-# 행 검사
+answer = 0
 for i in range(N):
-    if check(maps[i]):
-        count += 1
+    if count(maps[i]):
+        answer += 1
 
-# 열 검사
 for j in range(N):
     col = [maps[i][j] for i in range(N)]
-    if check(col):
-        count += 1
+    if count(col):
+        answer += 1
 
-print(count)
+print(answer)
